d692170195300cf70259d2b3d31fff5ef33d4f63
Kevin Riverbook <kevinriverbook@gmail.com>
2023-02-21 07:55:08 +0900

    notesテーブルを定義
    notesテーブルを定義しました。メモのタイトル、メモの本文、ユーザーidの3つのカラムを追加しました。

--- database/migrations/2023_02_21_072812_create_notes_table.php
+++ database/migrations/2023_02_21_072812_create_notes_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('note_title');                // メモのタイトル
+            $table->text('note_content');                // メモの本文
+            $table->foreignId('user_id')->constrained(); // ユーザーid(外部キー制約)
         });
     }
 
