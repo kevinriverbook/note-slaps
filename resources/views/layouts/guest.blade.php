35d641d9c5f34a3b6529ea2fe9e4e1dc9081f593
Kevin Riverbook <kevinriverbook@gmail.com>
2023-02-25 13:17:28 +0900

    認証ページのロゴを変更
    認証ページのロゴを変更しました。

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,8 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
+                    <x-application-logo class="block fill-current text-2xl text-gray-800 dark:text-gray-200 font-bold" />
                 </a>
             </div>
 
