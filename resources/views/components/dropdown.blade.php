a35e87ec4667fd19d6d17a9321bb27e07937a31c
Kevin Riverbook <kevinriverbook@gmail.com>
2023-02-26 23:14:23 +0900

    設定ドロップダウンを変更
    設定ドロップダウンを変更しました。

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,4 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700'])
+@props(['align' => 'right', 'width' => '52', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700'])
 
 @php
 switch ($align) {
@@ -17,6 +17,9 @@ switch ($align) {
 switch ($width) {
     case '48':
         $width = 'w-48';
+        break;
+    case '52':
+        $width = 'w-52';
         break;
 }
 @endphp
